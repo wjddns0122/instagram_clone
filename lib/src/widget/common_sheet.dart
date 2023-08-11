b9b1b0d7956eb6f51f4bdff3d057994333d86fb5
Jungwoon <jwoon2004@gmail.com>
2023-08-11 18:46:15 +0900

    저번주

---
+++ lib/src/widget/common_sheet.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/src/widget/image_data.dart';
+
+import 'image_avatar.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        _header(),
+        _body(),
+      ),
+    );
+  }
+
+  Widget _header() {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Colors.black),
+        ),
+      ),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('댓글'),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ImageData(path: ImagePath.directMessage),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _body() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 50,
+        itemBuilder: (context, index) => ListTile(
+          leading: const ImageAvatar(
+              url:
+                  'https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/pt/2023/05/22/202305221630774259_646b1a242bc47.jpg',
+              type: AvatarType.BASIC),
+          title: Text(
+            '$index user',
+            style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('$index번 째 댓글입니다.'),
+          trailing: ImageData(
+            path: ImagePath.activeOff,
+            width: 40,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/widget/feed.dart
+++ lib/src/widget/feed.dart
@@ -1,16 +1,25 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+import 'common_sheet.dart';
 import 'image_avatar.dart';
 import 'image_data.dart';
 
 class Feed extends StatefulWidget {
   final String userName;
   final List<String> urls;
-  const Feed({super.key, required this.userName, required this.urls});
+  final int countLikes;
+  final int countComment;
+  const Feed(
+      {super.key,
+      required this.userName,
+      required this.urls,
+      required this.countLikes,
+      required this.countComment});
 
   @override
   State<Feed> createState() => _FeedState();
@@ -25,6 +34,7 @@ class _FeedState extends State<Feed> {
         _header(),
         _images(),
         _options(),
+        _comment(),
         //_description(),
       ],
     );
@@ -95,20 +105,21 @@ class _FeedState extends State<Feed> {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ImageData(
-                path: ImagePath.activeOff,
+              child: GestureDetector(
+                child: ImageData(path: ImagePath.activeOff),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ImageData(
-                path: ImagePath.replyIcon,
+              child: GestureDetector(
+                onTap: showCommentSheet,
+                child: ImageData(path: ImagePath.replyIcon),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ImageData(
-                path: ImagePath.directMessage,
+              child: GestureDetector(
+                child: ImageData(path: ImagePath.directMessage),
               ),
             ),
           ],
@@ -148,4 +159,74 @@ class _FeedState extends State<Feed> {
       ],
     );
   }
+
+  Widget _comment() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '좋아요 ${widget.countLikes}개',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ExpandableText(
+              '컨텐츠 입니다. \n컨텐츠 입니다. \n컨텐츠 입니다. \n컨텐츠 입니다. \n컨텐츠 입니다.',
+              expandText: '더보기',
+              linkColor: Colors.grey,
+              prefixText: widget.userName,
+              prefixStyle:
+                  const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Text(
+                '댓글 ${widget.countComment}개 모두 보기',
+                style: const TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ImageAvatar(
+                  url:
+                      'https://postfiles.pstatic.net/MjAyMTA2MDlfMjM0/MDAxNjIzMjIyMjU5Mjgz.cv3La0LhNLcFnPJ091a8jHz6K8-UoA8BIQrZRZcJ54sg.z4v7OPd07iQ7gD7gj1I_WUxRjVxilKiwwvjV1uvHzhcg.PNG.sglucia_/%EB%9E%84%EB%A1%9C%EC%8D%AC%EA%B8%8002.png?type=w773',
+                  type: AvatarType.BASIC,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: const Text(
+                  '댓글 달기...',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showCommentSheet() => showModalBottomSheet(
+      // showDragHandle: true,
+      enableDrag: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25.0),
+          topRight: Radius.circular(25.0),
+        ),
+      ),
+      context: context,
+      builder: (context) => const CommentSheet());
 }
